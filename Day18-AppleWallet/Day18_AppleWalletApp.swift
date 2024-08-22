c10370207b7c498d8ed15dc991cb9a4711be3513
BeatrizECardozoLopez <beatrizecardozolopez@gmail.com>
2024-08-22 11:08:39 -0400

    Initial Commit

---
+++ Day18-AppleWallet/Day18_AppleWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day18_AppleWalletApp.swift
+//  Day18-AppleWallet
+//
+//  Created by Beatriz Cardozo on 22/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Day18_AppleWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
